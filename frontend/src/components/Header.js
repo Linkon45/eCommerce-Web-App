093e858cfa5263ad2cd1d0baf6623bb7cb8ab94e
Linkon <linkon3.1416@gmail.com>
2021-08-26 03:00:40 +0600

    Implement react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>eCommerce Website</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>eCommerce Website</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i>
-                <span className='ms-1'>Cart</span>
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>
-                <span className='ms-1'>Sign In</span>
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>
+                  <span className='ms-1'>Cart</span>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>
+                  <span className='ms-1'>Sign In</span>
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
